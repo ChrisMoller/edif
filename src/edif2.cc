b2f0c2f18035050f427a6d488b7d15629b11efe7
Chris Moller <moller@mollerware.com>
2018-07-27 09:18:33 -0400

    added detached-editor version, edif2

--- src/edif2.cc
+++ src/edif2.cc
@@ -18,64 +18,65 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#define USE_KIDS
+
+#include <error.h>
+#include <fcntl.h>
+#include <limits.h>
 #include <malloc.h>
+#include <mqueue.h>
+#include <poll.h>
+#include <signal.h>
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/inotify.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
-#include "Macro.hh"
 
 #include<iostream>
 #include<fstream>
 #include<string>
 
-#if 0
-#include "native/template.hh"
-#else
-#include "Value.icc"
-#include "Native_interface.hh"
+#include "Macro.hh"
 
-class NativeFunction;
+#include "edif2.hh"
 
-extern "C" void * get_function_mux(const char * function_name);
-static Token eval_ident_Bx(Value_P B, Axis x, const NativeFunction * caller);
-static Token eval_fill_B(Value_P B, const NativeFunction * caller);
-static Token eval_fill_AB(Value_P A, Value_P B, const NativeFunction * caller);
-
-Token
-eval_fill_B(Value_P B, const NativeFunction * caller)
-{
-UCS_string ucs("eval_fill_B() called");
-Value_P Z(ucs, LOC);
-   Z->check_value(LOC);
-   return Token(TOK_APL_VALUE1, Z);
-}
-
-Token
-eval_fill_AB(Value_P A, Value_P B, const NativeFunction * caller)
-{
-UCS_string ucs("eval_fill_B() called");
-Value_P Z(ucs, LOC);
-   Z->check_value(LOC);
-   return Token(TOK_APL_VALUE1, Z);
-}
-
-Token
-eval_ident_Bx(Value_P B, Axis x, const NativeFunction * caller)
-{
-UCS_string ucs("eval_ident_Bx() called");
-Value_P Z(ucs, LOC);
-   Z->check_value(LOC);
-   return Token(TOK_APL_VALUE1, Z);
-}
-#endif
+#define APL_SUFFIX ".apl"
+#define MQ_NAME "/WHATS_IT_TO_YOU"
+#define MQ_SIGNAL (SIGRTMAX - 2)
 
 #define EDIF_DEFAULT \
-  "emacs --geometry=40x20 -background '#ffffcc' -font 'DejaVu Sans Mono-10'"
+  "emacs --geometry=60x20 -background '#ffffcc' -font 'DejaVu Sans Mono-10'"
 
 using namespace std;
 
-char *dir = NULL;
+static pid_t watch_pid = -1;
+#ifdef USE_KIDS
+static pid_t *kids = NULL;
+static int kids_nxt = 0;
+static int kids_max = 0;
+#define KIDS_INCR 8
+#else
+static pid_t group_pid = 0;
+#endif
+static char *dir = NULL;
+static mqd_t mqd = -1;
+
+#ifdef USE_KIDS
+static void
+add_a_kid (pid_t kid)
+{
+  if (kids_max <= kids_nxt) {
+    kids_max += KIDS_INCR;
+    kids = (pid_t *)realloc (kids, kids_max * sizeof(pid_t));
+  }
+  kids[kids_nxt++] = kid;
+}
+#endif
 
 static bool
 close_fun (Cause cause, const NativeFunction * caller)
@@ -97,7 +98,126 @@ close_fun (Cause cause, const NativeFunction * caller)
     free (dir);
     dir = NULL;
   }
-  return true;
+
+  if (mqd != -1) mq_close (mqd);
+  mq_unlink (MQ_NAME);
+
+
+#ifdef USE_KIDS
+  int i;
+  for (i = 0; i < kids_nxt; i++) {
+    if (kids[i] > 0) kill (kids[i], SIGTERM);
+  }
+#else
+  if (group_pid > 0) {
+    killpg (group_pid, SIGTERM);
+    group_pid = 0;
+  }
+#endif
+
+  if (watch_pid > 0) {
+    kill (watch_pid, SIGTERM);
+    watch_pid = 0;
+  }
+  
+  return false;
+}
+
+/***
+    base_name == apl function name
+    fn = fully qualified file name
+***/
+
+static void
+read_file (const char *base_name, const char *fn)
+{
+  ifstream tfile;
+  tfile.open (fn, ios::in);
+  UCS_string ucs;
+  if (tfile.is_open ()) {
+    string line;
+    while (getline (tfile, line)) {
+      ucs.append_utf8 (line.c_str ());
+      ucs.append(UNI_ASCII_LF);
+    }
+    tfile.close ();
+    int error_line = 0;
+    UCS_string creator (base_name);
+    creator.append (UNI_ASCII_COLON);
+    creator.append_number (0);
+    UTF8_string creator_utf8(creator);
+    UserFunction::fix (ucs,		// text
+		       error_line,	// err_line
+		       false,		// keep_existing
+		       LOC,		// loc
+		       creator_utf8,	// creator
+		       true);		// tolerant
+  }
+}
+
+
+static void
+enable_mq_notify ()
+{
+  union sigval sv;
+  sv.sival_int = 0;
+  struct sigevent sevp;
+  sevp.sigev_notify = SIGEV_SIGNAL;
+  sevp.sigev_value  = sv;
+  sevp.sigev_signo  = MQ_SIGNAL;
+  int rc = mq_notify (mqd, &sevp);
+  if (rc == -1) perror ("mq_notify");
+}
+
+static void
+handle_msg ()
+{
+  char bfr[NAME_MAX + 8];
+  struct timespec ts = {.tv_sec = 0, .tv_nsec = 100000000};
+  ssize_t sz;
+  while (0 < (sz = mq_timedreceive(mqd, bfr, NAME_MAX + 8, NULL, &ts))) {
+    char *cpy = strdup (bfr);
+    if (cpy) {
+      char *suffix = &cpy[strlen (bfr) - strlen (APL_SUFFIX)];
+      if (!strcmp (suffix, APL_SUFFIX)) {
+	*suffix = 0;
+	char *fn = NULL;
+	asprintf (&fn, "%s/%s", dir, bfr);
+	if (fn) {
+	  read_file (cpy, fn);
+	  free (fn);
+	}
+      }
+      free (cpy);
+    }
+  }
+  enable_mq_notify ();
+}
+
+static void
+watch_chld_handler(int sig, siginfo_t *si, void *data)
+{
+  int wstatus;
+  waitpid (si->si_pid, &wstatus, WNOHANG);
+}
+
+static void
+edit_chld_handler(int sig, siginfo_t *si, void *data)
+{
+  int wstatus;
+  waitpid (si->si_pid, &wstatus, 0 /* WNOHANG */);
+#ifdef USE_KIDS
+  int i;
+  for (i = 0; i < kids_nxt; i++) {
+    if (kids[i] == si->si_pid) kids[i] = -1;
+  }
+#endif
+}
+
+static void
+msg_handler(int sig, siginfo_t *si, void *data)
+{
+  handle_msg ();
 }
 
 Fun_signature
@@ -106,6 +226,59 @@ get_signature()
   asprintf (&dir, "/var/run/user/%d/%d",
 	    (int)getuid (), (int)getpid ());
   mkdir (dir, 0700);
+
+  struct sigaction msg_act;
+  msg_act.sa_sigaction = msg_handler;
+  sigemptyset (&msg_act.sa_mask);
+  msg_act.sa_flags = SA_SIGINFO;
+  sigaction (MQ_SIGNAL, &msg_act, NULL);
+
+  struct sigaction chld_act;
+  chld_act.sa_sigaction = watch_chld_handler;
+  sigemptyset (&chld_act.sa_mask);
+  chld_act.sa_flags = SA_SIGINFO;
+  sigaction (SIGCHLD, &chld_act, NULL);
+
+  struct mq_attr attr;
+  attr.mq_maxmsg  = 8;	// no good reason
+  attr.mq_msgsize = NAME_MAX + 1;
+  mqd = mq_open (MQ_NAME, O_RDWR | O_CREAT /*| O_NONBLOCK*/,
+		 0600, &attr);
+  if (mqd == -1) perror ("mq_open");
+
+  char bfr[NAME_MAX + 8];
+  struct timespec ts = {.tv_sec = 0, .tv_nsec = 100000000};
+  ssize_t sz;
+  // empty the queue, just in case
+  while (0 < (sz = mq_timedreceive(mqd, bfr, NAME_MAX + 8, NULL, &ts))) {}
+  enable_mq_notify ();
+
+#ifndef USE_KIDS
+  group_pid = getpid ();
+#endif
+ 
+  pid_t pid = fork ();
+  if (pid > 0) watch_pid = pid;
+  else if (pid == 0) {		// child -- watch for file changes
+    
+    int inotify_fd = inotify_init ();
+    FILE *inotify_fp = fdopen(inotify_fd, "r");
+    int inotify_rc = inotify_add_watch (inotify_fd, dir, IN_MODIFY);
+    // int inotify_rc = inotify_add_watch (inotify_fd, dir, IN_ALL_EVENTS);
+    while (1) {
+#define BUF_LEN (10 * (sizeof(struct inotify_event) + NAME_MAX + 1))
+      char buf[BUF_LEN] __attribute__ ((aligned(8)));
+      ssize_t sz = read (inotify_fd, buf, BUF_LEN);
+      if (sz < 0) clearerr (inotify_fp);
+      else {
+	struct inotify_event *event = (struct inotify_event *)buf;
+	if (event->len > 0 && strlen (event->name) > 0) {
+	  int mrc = mq_send (mqd, event->name, event->len, 0);
+	  if (mrc == -1) perror ("mq_send");
+	}
+      }
+    }
+  }
 
   return SIG_Z_A_F2_B;
 }
@@ -150,12 +323,14 @@ get_fcn (const char *fn, const char *base, Value_P B)
       UTF8_string utf (line);
       tfile << utf << endl;
     }
+    tfile.flush ();
     tfile.close ();
   }
   else {
     ofstream tfile;
     tfile.open (fn, ios::out);
     tfile << base << endl;
+    tfile.flush ();
     tfile.close ();
   }
 }
@@ -179,7 +354,6 @@ cleanup (char *dir, UTF8_string base_name, char *fn)
   } 
   if (fn) free (fn);
 }
-
     /***
 	from NamedObject.hh:
 
@@ -199,51 +373,56 @@ eval_EB (const char *edif, Value_P B)
   if (B->is_char_string ()) {
     const UCS_string  ustr = B->get_UCS_ravel();
     UTF8_string base_name(ustr);
-
-#if 0
-#endif
     char *fn = NULL;
-    asprintf (&fn, "%s/%s.apl", dir, base_name.c_str ());
+    asprintf (&fn, "%s/%s%s", dir, base_name.c_str (), APL_SUFFIX);
 
     APL_Integer nc = Quad_NC::get_NC(ustr);
     switch (nc) {
     case NC_FUNCTION:
     case NC_UNUSED_USER_NAME:
       {
-	get_fcn (fn, base_name.c_str (), B);
-	char *buf;
-	asprintf (&buf, "%s %s", edif, fn);
-	system (buf);
-	if (buf) free (buf);
-
-	ifstream tfile;
-	tfile.open (fn, ios::in);
-	UCS_string ucs;
-	if (tfile.is_open ()) {
-	  string line;
-	  while (getline (tfile, line)) {
-	    ucs.append_utf8 (line.c_str ());
-	    ucs.append(UNI_ASCII_LF);
-	  }
-	  tfile.close ();
-	  int error_line = 0;
-	  UCS_string creator (base_name);
-	  creator.append (UNI_ASCII_COLON);
-	  creator.append_number (0);
-	  UTF8_string creator_utf8(creator);
-	  UserFunction::fix (ucs,		// text
-			     error_line,	// err_line
-			     false,		// keep_existing
-			     LOC,		// loc
-			     creator_utf8,	// creator
-			     true);		// tolerant
-	  cleanup (dir, base_name, fn);
-	}
-	else {
-	  UCS_string ucs ("Error opening working file.");
+	if (watch_pid < 0) {
+	  UCS_string ucs ("Internal failure.");
 	  Value_P Z (ucs, LOC);
 	  Z->check_value (LOC);
 	  return Token (TOK_APL_VALUE1, Z);
+	}
+	else {
+	  pid_t pid = fork ();
+	  if (pid < 0) {
+	    UCS_string ucs ("Editor process failed to fork.");
+	    Value_P Z (ucs, LOC);
+	    Z->check_value (LOC);
+	    return Token (TOK_APL_VALUE1, Z);
+	  }
+	  else if (pid > 0) {
+#ifdef USE_KIDS
+	    add_a_kid (pid);
+#else
+	    int rc = setpgid (pid, group_pid);
+	    if (rc == -1) perror ("setpgid");
+	    if (group_pid == 0) group_pid = getpgid (pid);
+#endif
+	    struct sigaction chld_act;
+	    chld_act.sa_sigaction = edit_chld_handler;
+	    sigemptyset (&chld_act.sa_mask);
+	    chld_act.sa_flags = SA_SIGINFO;
+	    sigaction (SIGCHLD, &chld_act, NULL);
+	  }
+	  else if (pid == 0) {
+	    get_fcn (fn, base_name.c_str (), B);
+	    char *buf;
+	    asprintf (&buf, "%s %s", edif, fn);
+	    int erc = execl("/bin/sh", "sh", "-c", buf, (char *) 0);
+	    if (erc == -1) {
+	      if (buf) free (buf);
+	      UCS_string ucs ("Editor process failed to execute.");
+	      Value_P Z (ucs, LOC);
+	      Z->check_value (LOC);
+	      return Token (TOK_APL_VALUE1, Z);
+	    }
+	  }
+	  cleanup (dir, base_name, fn);  // fixme
 	}
       }
       break;
---
+++ src/edif2.hh
@@ -0,0 +1,66 @@
+/*
+    This file is part of GNU APL, a free implementation of the
+    ISO/IEC Standard 13751, "Programming Language APL, Extended"
+
+    Copyright (C) 2008-2013  Dr. Jürgen Sauermann
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef EDIF_HH
+#define EDIF_HH
+
+/***
+    The following was nicked from native/template.hh because I needed
+    a custom close_fun().
+***/
+
+#include "Value.icc"
+#include "Native_interface.hh"
+
+class NativeFunction;
+
+extern "C" void * get_function_mux(const char * function_name);
+static Token eval_ident_Bx(Value_P B, Axis x, const NativeFunction * caller);
+static Token eval_fill_B(Value_P B, const NativeFunction * caller);
+static Token eval_fill_AB(Value_P A, Value_P B, const NativeFunction * caller);
+
+Token
+eval_fill_B(Value_P B, const NativeFunction * caller)
+{
+UCS_string ucs("eval_fill_B() called");
+Value_P Z(ucs, LOC);
+   Z->check_value(LOC);
+   return Token(TOK_APL_VALUE1, Z);
+}
+
+Token
+eval_fill_AB(Value_P A, Value_P B, const NativeFunction * caller)
+{
+UCS_string ucs("eval_fill_B() called");
+Value_P Z(ucs, LOC);
+   Z->check_value(LOC);
+   return Token(TOK_APL_VALUE1, Z);
+}
+
+Token
+eval_ident_Bx(Value_P B, Axis x, const NativeFunction * caller)
+{
+UCS_string ucs("eval_ident_Bx() called");
+Value_P Z(ucs, LOC);
+   Z->check_value(LOC);
+   return Token(TOK_APL_VALUE1, Z);
+}
+
+#endif  // EDIF_HH
